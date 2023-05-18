c68263750266c9c0bb8c07b17a65d37c4bf937d9
ckhawks <carter.hawks@agilevelocity.com>
2023-05-17 21:25:51 -0500

    ruined the loading code hopefully vercel loves it

--- src/lib/blog/get-posts.js
+++ src/lib/blog/get-posts.js
@@ -1,25 +1,46 @@
 import matter from 'gray-matter'
 import path from 'path'
 // import type { Post } from './types'
-import fs from 'fs/promises'
+import fs from 'fs'
+import { default as fs2 } from 'fs/promises';
 import { cache } from 'react'
 import { DateTime } from 'luxon';
 
 const POSTS_PATH = 'src/posts/';
 
+// import fs from 'fs'
+// import path from 'path'
+
+// export default (req, res) => {
+//   const dirRelativeToPublicFolder = 'img'
+
+//   const dir = path.resolve('./public', dirRelativeToPublicFolder);
+
+//   const filenames = fs.readdirSync(dir);
+
+//   const images = filenames.map(name => path.join('/', dirRelativeToPublicFolder, name))
+
+//   res.statusCode = 200
+//   res.json(images);
+// }
+
 // `cache` is a React 18 feature that allows you to cache a function for the lifetime of a request.
 // this means getPosts() will only be called once per page build, even though we may call it multiple times
 // when rendering the page.
 export const getPosts = cache(async () => {
-  const posts = await fs.readdir(POSTS_PATH);
+  const dir = path.resolve('./src/', 'posts'); // this line apparently tells vercel scanner that we need those files
+
+  const posts = fs.readdirSync(dir);
+
+  // const posts = await fs.readdir(POSTS_PATH);
   // console.log("posts files: ", posts)
 
   const unsortedPosts = await Promise.all(
     posts
       .filter((file) => path.extname(file) === '.mdx')  
       .map(async (file) => {
-        const filePath = `${POSTS_PATH}${file}`
-        const postContent = await fs.readFile(filePath, 'utf8')
+        const filePath = `${dir}/${file}`
+        const postContent = await fs2.readFile(filePath, 'utf8')
         const { data, content } = matter(postContent)
 
         if (data.published === false) {
